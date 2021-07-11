5fb72d8fb414cdbe47c73eec034bc9d950cd625f
Ian White <ian.w.white@gmail.com>
2021-07-11 13:06:14 +1000

    Simpler memoization

--- lib/i2w/memoize.rb
+++ lib/i2w/memoize.rb
@@ -1,68 +1,43 @@
 # frozen_string_literal: true
 
-require 'weakref'
-
 module I2w
-  # TODO: evaluate whether this is a bad idea (do some production testing), if it's OK, move into own gem
-  #
-  # enabes memoization, keeping references on the extended module or class, but with WeakRef keys,
-  # which can be garbage collected. This makes it possible to memoize methods on frozen objects.
-  #
-  # inspired by https://github.com/iliabylich/memoized_on_frozen/blob/master/lib/memoized_on_frozen.rb
+  # Simple memoization with handles frozen objects, and including modules with memoized methods
   module Memoize
     def self.extended(into)
-      into.instance_variable_set(:@_memoize_cache, WeakRefCache.new)
-      PrependSetWeakRefToClass.call(into)
+      PrependMemoizeCache.call(into)
     end
 
     def memoize(*method_names)
-      memoize_cache = @_memoize_cache
-      method_names.each do |meth|
-        orig = instance_method(meth)
-        remove_method(meth)
-        define_method(meth) do |*args|
-          cache = memoize_cache[@_weakref]
-          cache.fetch([meth, *args]) { cache.store [meth, *args], orig.bind(self).call(*args) }
+      method_names.each do |method_name|
+        orig_method = instance_method(method_name)
+
+        remove_method(method_name)
+
+        define_method(method_name) do |*args, **opts|
+          key = [method_name, args, opts]
+          @_memoize_cache.fetch(key) { @_memoize_cache[key] = orig_method.bind(self).call(*args, **opts) }
         end
       end
     end
 
-    def clear_memoization_for(obj) = @_memoize_cache[obj.instance_variable_get(:@_weakref)]&.clear
-
-    module PrependSetWeakRefToClass #:nodoc:
-      # prepend SetWeakRef to class, or if a module, set a hook to ensure that eventually happens
-      def self.call(into) = into.is_a?(Class) ? into.prepend(SetWeakRef) : into.singleton_class.prepend(Included)
+    module PrependMemoizeCache #:nodoc:
+      # prepend MemoizeCache to class, or if a module, set a hook to ensure that eventually happens
+      def self.call(into) = into.is_a?(Class) ? into.prepend(MemoizeCache) : into.singleton_class.prepend(Included)
 
       module Included #:nodoc:
-        def included(into) = super.tap { PrependSetWeakRefToClass.call(into) }
+        def included(into) = super.tap { PrependMemoizeCache.call(into) }
       end
     end
 
-    module SetWeakRef #:nodoc:
+    module MemoizeCache #:nodoc:
       def initialize(...)
-        @_weakref = WeakRef.new(self)
+        @_memoize_cache = {}
         super
-      end
-    end
-
-    # cache with weakref as keys, which are cleared on access if they are not alive.
-    class WeakRefCache #:nodoc:
-      def initialize
-        @cleared_at = GC.count
-        @cache = Hash.new { |cache, weakref| cache[weakref] = {} }
-      end
-
-      def [](weakref)
-        clear_dead_weakrefs! unless GC.count == @cleared_at
-        @cache[weakref]
       end
 
       private
 
-      def clear_dead_weakrefs!
-        @cache.select! { |k, _| k.weakref_alive? }
-        @cleared_at = GC.count
-      end
+      def _clear_memoization = @_memoize_cache.clear
     end
   end
 end
--- test/i2w/memoize_test.rb
+++ test/i2w/memoize_test.rb
@@ -13,9 +13,9 @@ module I2w
         @side_effects = side_effects
       end
 
-      memoize def foo(arg)
-        @side_effects << [self, :foo, arg]
-        "Foo: #{arg}"
+      memoize def foo(*args, **opts)
+        @side_effects << [self, :foo, args, opts]
+        "Foo: #{args.join(', ')}"
       end
     end
 
@@ -27,9 +27,9 @@ module I2w
         freeze
       end
 
-      memoize def foo(arg)
-        @side_effects << [self, :foo, arg]
-        "Foo: #{arg}"
+      memoize def foo(*args, **opts)
+        @side_effects << [self, :foo, args, opts]
+        "Foo: #{args.join(', ')}"
       end
     end
 
@@ -40,9 +40,9 @@ module I2w
       module Foo
         extend Memoize
 
-        memoize def foo(arg)
-          @side_effects << [self, :foo, arg]
-          "Foo: #{arg}"
+        memoize def foo(*args, **opts)
+          @side_effects << [self, :foo, args, opts]
+          "Foo: #{args.join(', ')}"
         end
       end
 
@@ -68,66 +68,28 @@ module I2w
         assert_equal 'Foo: 2', obj.foo(2)
         assert_equal 'Foo: 1', obj.foo(1)
         assert_equal 'Foo: 3', obj.foo(3)
+        assert_equal 'Foo: 3', obj.foo(3)
 
-        assert_equal [[obj, :foo, 1], [obj, :foo, 2], [obj, :foo, 3]], side_effects
+        assert_equal [ [obj, :foo, [1], {}],
+                       [obj, :foo, [2], {}],
+                       [obj, :foo, [3], {}] ], side_effects
+
+        obj.send(:_clear_memoization)
+
+        assert_equal 'Foo: 1', obj.foo(1)
+        assert_equal [ [obj, :foo, [1], {}],
+                       [obj, :foo, [2], {}],
+                       [obj, :foo, [3], {}],
+                       [obj, :foo, [1], {}] ], side_effects
+
+        assert_equal 'Foo: 1', obj.foo(1, busted: true)
+
+        assert_equal [ [obj, :foo, [1], {}],
+                       [obj, :foo, [2], {}],
+                       [obj, :foo, [3], {}],
+                       [obj, :foo, [1], {}],
+                       [obj, :foo, [1], { busted: true }] ], side_effects
       end
-    end
-
-    test 'memoization goes away after GC' do
-      GC.start
-
-      obj1 = Test.new
-      obj2 = Test.new
-
-      assert_equal 'Foo: 1', obj1.foo(1)
-      assert_equal 'Foo: 2', obj2.foo(2)
-      assert_equal 'Foo: 1', obj1.foo(1)
-
-      assert_equal 2, Test.instance_variable_get(:@_memoize_cache).instance_eval { @cache }.length
-
-      obj1 = nil
-      GC.start
-
-      assert_equal 'Foo: 2', obj2.foo(2)
-      assert_equal 1, Test.instance_variable_get(:@_memoize_cache).instance_eval { @cache }.length
-    end
-
-    test 'clear_memoization_for(obj)' do
-      GC.start
-
-      obj1 = Test.new
-      assert_equal 'Foo: 1', obj1.foo(1)
-
-      assert_equal [{ [:foo, 1] => 'Foo: 1' }],
-                   Test.instance_variable_get(:@_memoize_cache).instance_eval { @cache }.values
-
-      Test.clear_memoization_for(obj1)
-
-      assert_equal [{}], Test.instance_variable_get(:@_memoize_cache).instance_eval { @cache }.values
-    end
-
-    test 'memoization of immutable objects' do
-      GC.start
-      obj = ImmutableTest.new
-
-      assert_equal 'Foo: 1', obj.foo(1)
-      assert_equal 'Foo: 1', obj.foo(1)
-
-      assert_equal [{ [:foo, 1] => 'Foo: 1' }], ImmutableTest.instance_variable_get(:@_memoize_cache).instance_eval {
-                                                  @cache
-                                                }.values
-    end
-
-    test 'memoization of module based methods' do
-      GC.start
-      obj = ModuleTest.new
-
-      assert_equal 'Foo: 1', obj.foo(1)
-      assert_equal 'Foo: 1', obj.foo(1)
-
-      assert_equal [{ [:foo, 1] => 'Foo: 1' }], ModuleTest::Foo.instance_variable_get(:@_memoize_cache).instance_eval {
-                                                  @cache
-                                                }.values
     end
   end
 end
