944260a4d68a0d477974eebba227ad9cd0e3fb5d
Ian White <ian.w.white@gmail.com>
2021-12-21 21:25:05 +1100

    Document Memoization shenanigans

--- lib/i2w/memoize.rb
+++ lib/i2w/memoize.rb
@@ -6,21 +6,21 @@ module I2w
     def self.extended(object) = Attach.call(object)
 
     def memoize(*method_names)
-      method_names.each do |method_name|
-        visibility = (private_method_defined?(method_name) && 'private') ||
-                     (protected_method_defined?(method_name) && 'protected') || 'public'
+      method_names.each do |meth|
+        visibility = (private_method_defined?(meth) && 'private') ||
+                     (protected_method_defined?(meth) && 'protected') || 'public'
 
-        alias_method "_unmemoized_#{method_name}", method_name
-        remove_method method_name
+        alias_method "_unmemoized_#{meth}", meth
+        remove_method meth
 
         module_eval <<~end_ruby, __FILE__, __LINE__
-          #{visibility}                                                 # protected
-                                                                        #
-          def #{method_name}(*a, **kw)                                  # def foo(*a, **kw)
-            _memoize_cache.fetch [:#{method_name}, a, kw] do            #   _memoize_cache.fetch [:foo, a, kw] do
-              _memoize_cache[_1] = _unmemoized_#{method_name}(*a, **kw) #     _memoize_cache[_1] = _unmemoized_foo(*a, **kw)
-            end                                                         #   end
-          end                                                           # end
+          #{visibility}                                           # protected
+                                                                  #
+          def #{meth}(*a, **kw)                                   # def foo(*a, **kw)
+            _memoize_cache.fetch [:#{meth}, a, kw] do             #   _memoize_cache.fetch [:foo, a, kw] do
+              _memoize_cache[_1] = _unmemoized_#{meth}(*a, **kw)  #     _memoize_cache[_1] = _unmemoized_foo(*a, **kw)
+            end                                                   #   end
+          end                                                     # end
         end_ruby
       end
     end
@@ -31,7 +31,7 @@ module I2w
         return object.include(ForSingleton) if object.singleton_class?
         return object.prepend(ForClass)     if object.instance_of?(Class)
 
-        # object is a module: prepend this, so that it extends or includes memoization setup
+        # object is a module: prepend this module, so that it extends or includes memoization setup
         object.singleton_class.prepend(Attach)
       end
 
@@ -40,6 +40,7 @@ module I2w
       def extended(object) = super.tap { object.extend(ForSingleton) }
     end
 
+    # Prepended to classes, to setup the memoization cache before anything else (such as becoming frozen)
     module ForClass #:nodoc:
       def initialize(...)
         @_memoize_cache = {}
@@ -51,6 +52,7 @@ module I2w
       attr_reader :_memoize_cache
     end
 
+    # Singleton classes don't get initialized, so we set up a lazy reader for the memoization cache
     module ForSingleton #:nodoc:
       private
 
